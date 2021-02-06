923844010dcdf2bd9cffc8b4e410006e36161e13
DesmondM <desmondmpofu@gmail.com>
2021-02-06 21:22:21 +0200

    Updates

---
+++ precache-manifest.9bc476a2eb4ad848ca0d0681651d77d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b931b7298aae152318acbd3cae406671",
+    "url": "/react_deploy2/index.html"
+  },
+  {
+    "revision": "6bcecece3d5f44bdd159",
+    "url": "/react_deploy2/static/css/main.bb3ac692.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/react_deploy2/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_deploy2/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcecece3d5f44bdd159",
+    "url": "/react_deploy2/static/js/main.6109d158.chunk.js"
+  },
+  {
+    "revision": "c715311bdfedaa9cf6cb",
+    "url": "/react_deploy2/static/js/runtime-main.26eb68c3.js"
+  }
+]);
